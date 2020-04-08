d41646eff063824bab930d1f390fb4011711223e
sudeesh baskaran <sudeesh.b26@.com>
2020-04-08 19:04:54 +0100

    added routing changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+
+const Hats = () => {
+  return <h1>Hats</h1>;
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/Hats" component={Hats} />
+      </Switch>
     </div>
   );
 }
