b52db62dc08a5c14e82a4c4dc6e155adf90e8643
sudeesh baskaran <sudeesh.b26@.com>
2020-04-26 19:49:52 +0100

    added carticon and cart dropdown component and toggle is handled via redux

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { auth } from "../firebase/firbase.utils";
 import { connect } from "react-redux";
+import CartIcon from "../cart-icon/cartIcon.component";
 
 import { ReactComponent as Logo } from "../../assets/original.svg";
 
 import "./header.styles.scss";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link to="/" className="logo-container">
       <Logo className="logo"></Logo>
@@ -28,12 +30,15 @@ const Header = ({ currentUser }) => (
           Sign In
         </Link>
       )}
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 
 import userReducer from "./users/user.reducer";
+import CartReducer from "./cart/cart.reducer";
 
 export default combineReducers({
   user: userReducer,
+  cart: CartReducer,
 });
